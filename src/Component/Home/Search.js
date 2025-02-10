c6bee92d7cacf7e408dbaedf21ef86e3a01e959f
Vikas-Prince <vikasprince30809@gmail.com>
2025-02-10 23:15:50 +0530

    Created Home Component with Location Search

---
+++ src/Component/Home/Search.js
@@ -0,0 +1,76 @@
+import { useState, useEffect } from "react";
+import "./Search.css";
+
+const base_url = process.env.REACT_APP_API_URL;
+const Search = () => {
+  const [location, setLocation] = useState([]);
+  const [restaurants, setRestaurants] = useState([]);
+
+  useEffect(() => {
+    //api call on page load
+    fetch(`${base_url}/location`, { method: "GET" })
+      //return promise
+      .then((res) => res.json())
+      // return data
+      .then((data) => {
+        console.log(data);
+        setLocation(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const renderCity = (data) => {
+    if (data) {
+      return data.map((item) => {
+        return (
+          <option key={item._id} value={item.state_id}>
+            {item.state}
+          </option>
+        );
+      });
+    }
+  };
+
+  const handleCity = (event) => {
+    let stateId = event.target.value;
+    fetch(`${base_url}/restaurant?stateId=${stateId}`, { method: "GET" })
+      .then((res) => res.json())
+      .then((data) => {
+        setRestaurants(data);
+      });
+  };
+
+  const renderRest = (data) => {
+    if (data) {
+      return data.map((item) => {
+        return (
+          <option key={item.restaurant_id} value={item.restaurant_id}>
+            {item.restaurant_name} | {item.address}
+          </option>
+        );
+      });
+    }
+  };
+
+  return (
+    <div className="search">
+      <div id="logo">
+        <span>D!</span>
+      </div>
+      <div id="heading">Search Place Near To You</div>
+      <div id="dropdown">
+        <select id="city-dropdown" onChange={handleCity}>
+          <option>----SELECT CITY----</option>
+          {renderCity(location)}
+        </select>
+        <select className="restSelect">
+          <option>----SELECT Restaurants----</option>
+          {renderRest(restaurants)}
+        </select>
+      </div>
+    </div>
+  );
+};
+export default Search;
--- src/Component/Routing.js
+++ src/Component/Routing.js
@@ -1,11 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
-
+import Main from "./Main";
+import Home from "./Home/Home";
 const Routing = () => {
   return (
     <BrowserRouter>
       <Header />
+      <Routes>
+        <Route path="/" element={<Main />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
       <Footer />
     </BrowserRouter>
   );
