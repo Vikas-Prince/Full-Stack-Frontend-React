ba763dbac21468caac99bef8f5136cdbbc01dd81
Vikas-Prince <vikasprince30809@gmail.com>
2025-04-02 02:30:13 +0530

    Created a basic test case to verify using Jtest

---
+++ basic.test.js
@@ -0,0 +1,11 @@
+// basic.test.js
+import React from "react"; // <-- Add this line
+
+import { render } from "@testing-library/react";
+import HelloWorld from "./src/test";
+
+test("renders HelloWorld component", () => {
+  const { getByText } = render(<HelloWorld />);
+  const linkElement = getByText(/Hello, World!/i);
+  expect(linkElement).toBeInTheDocument();
+});
---
+++ src/test.js
@@ -0,0 +1,11 @@
+// HelloWorld.js
+import React from "react";
+
+const HelloWorld = () => {
+  return <h1>Hello, World!</h1>;
+};
+test("dummy test", () => {
+  expect(true).toBe(true);
+});
+
+export default HelloWorld;
